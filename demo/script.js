b7e27b4fa6b754c061d6b6ebdb51abe3c6d27cfa
蔡雪丹 <caixuedan@corp.netease.com>
2018-07-31 11:02:12 +0800

    fix:image inline style

--- demo/script.js
+++ demo/script.js
@@ -1,6 +1,50 @@
 var quill = new Quill('#editor', {
 	theme: 'snow',
 	modules: {
-		imageResize: {}
-	}
+		toolbar: [
+			['bold', 'italic', 'underline', 'strike',
+				{
+					'script': 'sub',
+				}, {
+					'script': 'super',
+				},
+				{
+					'color': [],
+				}, {
+					'background': [],
+				}, 'code',
+			],
+			[{
+				'font': [],
+			}, {
+				'size': ['small', false, 'large', 'huge'],
+			}],
+
+			['link', 'image', 'video', 'formula'],
+			['blockquote', 'code-block', {
+				'header': 1,
+			},
+				{
+					'list': 'ordered',
+				}, {
+					'list': 'bullet',
+				},
+			],
+			[{
+				'indent': '-1',
+			}, {
+				'indent': '+1',
+			}, {
+				'direction': 'rtl',
+			}, {
+				'align': [],
+			}],
+			['clean'],
+		],
+		imageResize: {},
+	},
 });
+
+function convert() {
+	console.log(quill.getContents());
+}
